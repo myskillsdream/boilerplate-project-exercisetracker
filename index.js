b0f1d93174193d1aca46c957a5e5056eac54c83b
myskillsdream <myskillsdream@gmail.com>
2022-08-06 17:10:35 +0100

    make api

--- index.js
+++ index.js
@@ -108,100 +108,104 @@ app.post("/api/users/:id/exercises", (req, res) => {
   })
 })
 
-// app.get("/api/users/:id/logs", (req, res) => {
-//   const { from, to, limit } = req.query;
-//   const {id} = req.params;
-//   User.findById(id, (err, userData) => {
-//     if(err || !userData) {
-//       res.send("Could not find user");
-//     }else{
-//       let dateObj = {}
-//       if(from){
-//         dateObj["$gte"] = new Date(from)
-//       }
-//       if(to){
-//         dateObj["$lte"] = new Date(to)
-//       }
-//       let filter = {
-//         userId: id
-//       }
-//       if(from || to ){
-//         filter.date = dateObj
-//       }
-//       let nonNullLimit = limit ? limit : 500;
-//       Exercise.find(filter).limit(nonNullLimit).exec((err, data) => {
-//         if(err || !data){
-//           res.json([])
-//         }else{
+app.get("/api/users/:id/logs", (req, res) => {
+  const { from, to, limit } = req.query;
+  const {id} = req.params;
+  User.findById(id, (err, userData) => {
+    if(err || !userData) {
+      res.send("Could not find user");
+    }else{
+      let dateObj = {}
+      if(from){
+        dateObj["$gte"] = new Date(from)
+      }
+      if(to){
+        dateObj["$lte"] = new Date(to)
+      }
+      let filter = {
+        userId: id
+      }
+      if(from || to ){
+        filter.date = dateObj
+      }
+      // let nonNullLimit = limit ? limit : 500;
+      let limitPage = (limit !== '' ? parseInt(limit) : 500);
 
-//           let count = data.length
+      Exercise.find(filter).limit(limitPage).exec((err, data) => {
+        if(err || !data){
+          res.json([])
+        }else{
 
-//           const rawLog = data
-//           const {username, _id} = userData;
-//           const log = rawLog.map((l) => ({
-//             description: l.description,
-//             duration: l.duration,
-//             date: l.date.toDateString()
-//           }))
-//           res.json({username, count, _id, log})
+          let count = data.length
 
-//           console.log(count)
-//         }
-//       })
-//     } 
-//   })
-// })
+          const rawLog = data
+          const {username, _id} = userData;
+          const log = rawLog.map((l) => ({
+            description: l.description,
+            duration: l.duration,
+            date: l.date.toDateString()
+          }))
+          res.json({username, count, _id, log})
 
-
-app.get('/api/users/:id/logs', async function(req, res) {
-const { id } = req.params;
-let { from, to, limit } = req.query;
-
-console.log('/api/users/:id/logs', id, from, to, limit)
-
-try {
-let findConditions = { uid: id };
-
-if ((from !== undefined && from !== '') || (to !== undefined && to !== '')) {
-  findConditions.date = {};
-
-  if (from !== undefined && from !== '') {
-    findConditions.date.$gte = new Date(Date.parse(from));
-  }
-
-  if (to !== undefined && to !== '') {
-    findConditions.date.$lte = new Date(Date.parse(to));
-  }
-}
-
-let limitPage = (limit !== '' ? parseInt(limit) : 0);
-
-const data = await Exercise
-  .find(findConditions)
-  .limit(limitPage)
-  .exec();
-
-const response = {
-  username: data[0].username,
-  count: data.length,
-  id: data[0].uid,
-  log: data.map((item) => ({
-    description: item.description,
-    duration: item.duration,
-    date: item.date.toDateString()
-  }))
-}
-
-console.log('/api/users/:id/logs', response)
-
-return res.status(200).json(response)
-} catch (err) {
-res.status(500).json({
-message: err
-});
-}
-
+          console.log(count)
+        }
+      })
+    } 
+  })
 })
+
+
+// app.get('/api/users/:id/logs', async function(req, res) {
+// const { id } = req.params;
+// let { from, to, limit } = req.query;
+
+// console.log('/api/users/:id/logs', id, from, to, limit)
+
+// try {
+// let findConditions = { uid: id };
+
+// if ((from !== undefined && from !== '') || (to !== undefined && to !== '')) {
+//   findConditions.date = {};
+
+//   if (from !== undefined && from !== '') {
+//     findConditions.date.$gte = new Date(Date.parse(from));
+//   }
+
+//   if (to !== undefined && to !== '') {
+//     findConditions.date.$lte = new Date(Date.parse(to));
+//   }
+// }
+
+
+// let limitPage = (limit !== '' ? parseInt(limit) : 0);
+
+// const data = await Exercise
+//   .find(findConditions)
+//   .limit(limitPage)
+//   .exec();
+
+// const response = {
+//   username: data[0].username,
+//   count: data.length,
+//   id: data[0].userId,
+//   log: data.map((item) => ({
+//     description: item.description,
+//     duration: item.duration,
+//     date: item.date.toDateString()
+//   }))
+  
+// }
+
+// console.log('/api/users/:id/logs', response)
+
+// return res.status(200).json(response)
+// } catch (err) {
+// res.status(500).json({
+// message: err
+// });
+// }
+
+// });
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
